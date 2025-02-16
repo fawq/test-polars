ee88e07bb80e542e55430c2a323537105a142dfb
Krystian Krakowski <kkrakowski22@gmail.com>
2025-02-16 13:15:02 +0100

    Refactore code

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Run all the possible variants.
+#
+# Usage: ./run.sh [--python|--python-module|--rust|--rust-release|--all] <request.csv> <response.csv>
+#
+# Examples:
+#   ./run.sh --python test/data/request.csv test/data/response.csv
+#   ./run.sh --python-module test/data/request.csv test/data/response.csv
+#   ./run.sh --rust test/data/request.csv test/data/response.csv
+#   ./run.sh --rust-release test/data/request.csv test/data/response.csv
+#   ./run.sh --all test/data/request.csv test/data/response.csv
 main() {
     if [ "$#" -ne 3 ]; then
-        echo "Usage: $0 [--python|--rust] <request.csv> <response.csv>"
+        echo "Usage: $0 [--python|--python-module|--rust|--rust-release|--all] <request.csv> <response.csv>"
         exit 1
     fi
 
